a9a9b04ce11d600656f12343ed176a8555c2306e
Jihee <sjh709@naver.com>
2024-01-08 02:13:41 +0900

    react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Homepage from './page/Homepage';
+import Aboutpage from './page/Aboutpage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<Aboutpage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
